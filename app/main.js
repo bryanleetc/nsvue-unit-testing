af156f4aebdb322bdf86df808dbe92a29ae628b5
Bryan Lee <bryan.lee@shopify.com>
2019-10-19 11:21:20 +0200

    Initial commit

---
+++ app/main.js
@@ -0,0 +1,15 @@
+import Vue from 'nativescript-vue'
+import App from './components/App'
+
+
+
+
+  
+// Prints Vue logs when --env.production is *NOT* set while building
+Vue.config.silent = (TNS_ENV === 'production')
+
+
+new Vue({
+  
+  render: h => h('frame', [h(App)])
+}).$start()
